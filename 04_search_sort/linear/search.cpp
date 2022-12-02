14e20e3b63cf13830505020e4c831548ebfa3566
Ilya1527 <ilyashcherbenok@gmail.com>
2022-12-02 21:34:08 +0300

    feat(04_search_sort): linear

---
+++ 04_search_sort/linear/search.cpp
@@ -0,0 +1,13 @@
+int search(int arr[], int n, int x, int result[]) 
+{
+    int b = 0;
+    for (int i = 0; i <= n; i++) 
+    {
+        if (arr[i] == x) 
+        {
+            result[b] = i;
+            b++;
+        }
+    }
+    return b;
+}
